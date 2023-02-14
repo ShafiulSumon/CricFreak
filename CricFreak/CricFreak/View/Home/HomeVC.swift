71a20f3576895878747292544efc800f805f0769
Shafiul Alam <shafiul.alam@bjitacademy.com>
2023-02-14 19:58:34 +0600

    segmented control design is done. #v1.0.3

--- CricFreak/CricFreak/View/Home/HomeVC.swift
+++ CricFreak/CricFreak/View/Home/HomeVC.swift
@@ -9,16 +9,48 @@ import UIKit
 
 class HomeVC: UIViewController {
     
-//    override var preferredStatusBarStyle: UIStatusBarStyle {
-//        return .lightContent // set the status bar style to light content
-//    }
+//MARK: - Outlets
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    @IBOutlet weak var topView: UIView!
     
+//MARK: - WillAppear
     override func viewWillAppear(_ animated: Bool) {
-        UIApplication.shared.statusBarStyle = .lightContent        
+        UIApplication.shared.statusBarStyle = .lightContent
     }
 
-    
+//MARK: - ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setUpSegmentControl()
     }
+    
+//MARK: - Button Actions
+    @IBAction func didChangeSegmentedValue(_ sender: UISegmentedControl) {
+        if(sender.selectedSegmentIndex == 0) {
+
+        }
+        else if(sender.selectedSegmentIndex == 1) {
+            
+        }
+        else if(sender.selectedSegmentIndex == 2) {
+            
+        }
+        else if(sender.selectedSegmentIndex == 3){
+            
+        }
+    }
+    
+//MARK: - All functions
+    func setUpSegmentControl() {
+        segmentControl.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.lightGray], for: .normal)
+        segmentControl.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.white], for: .selected)
+        
+        let unselectedBackgroundImage = UIImage.imageWithColor(color: .clear, size: CGSize(width: 1, height: 32))
+        segmentControl.setBackgroundImage(unselectedBackgroundImage, for: .normal, barMetrics: .default)
+
+        let selectedBackgroundImage = UIImage.imageWithColor(color: UIColor(named: "customBlue")!, size: CGSize(width: 1, height: 32)).imageWithBorder(width: 0, color: .blue, position: .bottom)
+        segmentControl.setBackgroundImage(selectedBackgroundImage, for: .selected, barMetrics: .default)
+    }
+    
 }
